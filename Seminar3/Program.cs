92d15e3b2000e2720223204a51403e20ba69390e
Igor Panfilov <i26p@ya.ru>
2023-02-18 16:15:26 +0300

    Seminar3

---
+++ Seminar3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Console.WriteLine("Введите номер четверти: ");
+// int q=int.Parse(Console.ReadLine());
+// if (q==1)
+// Console.WriteLine("X>0 и Y>0");
+// else if (q==2)
+// Console.WriteLine("X<0 и Y>0");
+// else if (q==3)
+// Console.WriteLine("X<0 и Y<0");
+// else if (q==4)
+// Console.WriteLine("X>0 и Y<0");
+// else Console.WriteLine("Введена неправильная четверть");
+
+// Console.WriteLine("Введите X1: ");
+// double X1=double.Parse(Console.ReadLine());
+// Console.WriteLine("Введите Y1: ");
+// double Y1=double.Parse(Console.ReadLine());
+// Console.WriteLine("Введите X2: ");
+// double X2=double.Parse(Console.ReadLine());
+// Console.WriteLine("Введите Y2: ");
+// double Y2=double.Parse(Console.ReadLine());
+// double s = Math.Sqrt((X1 - X2)*(X1 - X2) + (Y1-Y2)*(Y1-Y2));
+// Console.WriteLine();
+// Console.WriteLine($"{s}");
+
+// Console.WriteLine("Введите число: ");
+// int n = int.Parse(Console.ReadLine());
+// int result =1;
+// for(int i=1;i<=n;i++){
+// result = i*i;
+// Console.Write($"{result}, ");
+// }
+
+
