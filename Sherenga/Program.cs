9b7bda4b3fc69cf907ccec767e155bb1dabab5cf
Igor Panfilov <i26p@ya.ru>
2023-02-18 13:49:19 +0300

    Сортировка

--- Sherenga/Program.cs
+++ Sherenga/Program.cs
@@ -12,21 +12,11 @@ Array.Sort(array);
 Array.Reverse(array);
 //Console.WriteLine();
  for (int i = 0; i < n; i++)
-               {
-                    Console.Write($"{array[i]} ");
-               }
+ {
+Console.Write($"{array[i]} ");
+}
                
-// int n = 8;
-// int Petya = 162;
-// // int[]class = {165,163,160,160,157,157,155,154};
-// // int n = class.Length;
-// // for (int i=0; i<n; i++)
-// // Console.Write({class[i]});
 
-//     // if (array[i]<array[i+1]){
-//     // // int b = array[i];
-    // // array[i] = array[i+1];
-    // array[i+1] = b;
     
    
    
